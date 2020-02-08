63dc3eff4ade679b8f44eb045fb2521d4f37ea07
Brendan Powers <brendanpowers@Brendans-MBP.domain.local>
2020-02-07 21:29:32 -0500

    update

--- terminal/index.js
+++ terminal/index.js
@@ -2,10 +2,13 @@ const socket = require("socket.io-client")("http://localhost:3000");
 const repl = require("repl");
 
 let username = "";
+const randomNames = ["bran", "alex", "sean", "travis"];
 
 socket.on("connect", () => {
     console.log("_______Start Chatttttttttting________")
     username = process.argv[2];
+    if (typeof username === "undefined")
+        username = randomNames[]
 })
 
 socket.on("message", (message) => {
